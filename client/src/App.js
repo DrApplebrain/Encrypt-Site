0b49aed05e4184f9fd39917ed43dd070da322d4e
kai christensen <kai.christensen@dci-student.org>
2025-12-09 23:59:56 +0100

    update react

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import { Navigate, Route, Routes } from "react-router-dom"
-import Home from './page/Home';
-import AsciiPage from './page/AsciiPage.jsx';
-import QRPage from './page/QRPage.jsx';
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import Home from "./page/Home";
+import AsciiPage from "./page/AsciiPage.jsx";
+import QRPage from "./page/QRPage.jsx";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Routes>
-      <Route path="/"/>
-      <Route index element={<Home />} />
-      <Route path="ascii" element={<AsciiPage />} />
-      <Route path="qr" element={<QRPage />} />
+          <Route path="/" />
+          <Route index element={<Home />} />
+          <Route path="ascii" element={<AsciiPage />} />
+          <Route path="qr" element={<QRPage />} />
       </Routes>
+      </BrowserRouter>
     </div>
   );
 }
