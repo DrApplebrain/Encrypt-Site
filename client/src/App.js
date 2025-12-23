6f75183d0f8bf5502e78a2e99ab0b86ad3695e12
kai christensen <kai.christensen@dci-student.org>
2025-12-23 02:00:41 +0100

    deployment8

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
-import { Navigate, Route, Routes } from "react-router-dom"
-import Home from './page/Home';
-import AsciiPage from './page/AsciiPage.jsx';
-import QRPage from './page/QRPage.jsx';
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import AsciiPage from "./page/AsciiPage.jsx";
+import QRPage from "./page/QRPage.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-      <Route path="/"/>
-      <Route index element={<Home />} />
-      <Route path="ascii" element={<AsciiPage />} />
-      <Route path="qr" element={<QRPage />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route index element={<Home />} />
+          <Route path="ascii" element={<AsciiPage />} />
+          <Route path="qr" element={<QRPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
